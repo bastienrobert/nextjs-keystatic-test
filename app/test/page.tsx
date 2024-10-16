7df4d47593cc692fe88769b00db60566bd40beea
Bastien Robert <bastienrobert@outlook.com>
2024-10-16 17:42:26 +0200

    feat: add a test folder

---
+++ app/test/page.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { reader } from '@/lib/reader';
+
+export default async function Page() {
+  const posts = await reader().collections.posts.all();
+
+  return (
+    <div className="app">
+      <h1 className="mb-4 font-semibold text-2xl">
+        Next JS + Keystatic example
+      </h1>
+
+      <p className="mb-4">
+        With Github integration and draft mode. Edit content{' '}
+        <Link href={'/keystatic'}>here</Link>.
+      </p>
+
+      <ul className="list-disc mb-8 pl-4">
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/fr/posts/${post.slug}`}>{post.entry.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
